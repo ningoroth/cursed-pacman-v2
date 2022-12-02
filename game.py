d22b64458fd35837e75ea6f1b168fad13c53f6a1
Terkel <tiri0003@gmail.com>
2022-12-02 09:08:00 +0100

    Skriver forklaringer

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 ## REQUIRED MODULES ##
 # Importere biblioteker #
-
 import random
 import time
 import pygame as pg
@@ -9,8 +8,9 @@ from pacman import PacMan
 from ghost import Ghost
 from level import Level
 
+
 ## SETUP ##
-# Opstiller spillets "level" og sætter x,y koordinater for spilskærmen#
+# Opstiller spillets "level" og scaler x,y koordinater til spilskærmen#
 pg.init()
 level = Level("level.txt")
 
@@ -20,29 +20,34 @@ screen = pg.display.set_mode((width, height))
 
 # Opstiller startskærm og vinduets navn #
 pg.display.set_caption("Fucked up Pac-Man")
-font_press_enter = pg.font.Font(None, 32)
+font = pg.font.Font(None, 32)
+
 
 ## GAME LOOP ##
-# Opstiller 
+# Klargøre variabler til gameloop #
 direction = None
 tick = 0
 state = "LOAD"
 points = 0
 running = True
+
+# WhileLoop/GameLoop #
 while running:
     
+    # Initiere classes til variabler #
     if state == "LOAD":
         pacman = PacMan(level.pacman_x, level.pacman_y)
         ghost = Ghost(level.ghost_x, level.ghost_y)
         direction = None
         state = "READY"
 
-
+    # Loader startskærmen (Font, størrelse og tekst) #
     elif state == "READY":
-        text = font_press_enter.render("Press [Enter] to play", True, (220,220,10))
+        text = font.render("Press [Enter] to play", True, (220,220,10))
         text_rect = text.get_rect(center=(8*32/2, 7*32/2)) 
         screen.blit(text, text_rect)
 
+        # Definerer "enter" og "exit" for at starte og slutte spillet # 
         events = pg.event.get()
         for event in events:
             if event.type == pg.QUIT:
@@ -51,20 +56,22 @@ while running:
                 if event.key == pg.K_RETURN:
                     state = "PLAY"  
 
+        # Opdatere display og hele programmet delayer i 0.1 sekundt #
         pg.display.flip()  
         time.sleep(0.1)
         
-
+    # Hvis man er igang med spillet "play", så sker understående: #
     elif state == "PLAY": 
 
-        ## Handle events (keypresses etc.)
+        ## HANDLE EVENTS (keypresses etc.) ##
         events = pg.event.get()
         for event in events:
 
-            # Close window (e.g. pressing [x] or Ctrl+F4)
+            # Lukker vinduet, mens man spiller (e.g. pressing [x] or Ctrl+F4) #
             if event.type == pg.QUIT:
                 running = False
-            # Keypresses
+            
+            # Keyboard defineringer, controls (som vi bruger i pacman.py) #
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_UP:
                     direction = "up"
@@ -78,23 +85,33 @@ while running:
                     running = False
 
 
-        ## Move / logic ##
+        ## MOVE / LOGIC ##
+        # Kalder variablernes movefunktion #
+        # level og direction gør det muligt at 
+        # anvende i pacman og ghost funktioner #
         pacman.move(level, direction)
-
         ghost.move(level)
 
-
+        # Hvis pacman er i x,y koordinatet for "." #
+        # vil point += 1, og ryder feltet " " #
         if level.tiles[pacman.row][pacman.col] == ".":
             points += 1
-            print("points:", points)
             level.tiles[pacman.row][pacman.col] = " "
 
-
         ## Draw ##
+        # Fylder skærmen til sort, variablerne tegner classerne #
         screen.fill((0,0,0))
         level.draw(screen)
         ghost.draw(screen)
         pacman.draw(screen,direction)
+
+        ## POINT RENDER ##
+        # Render font og skriver tekst #
+        # Insætter tekst i rectangle, med spawnpoint i venstre hjørne #
+        # Indsætter tekst på skærmen #
+        pointsText = font.render(f"Points: {points}", True, (220, 220, 10))
+        pointsText_rect = pointsText.get_rect(bottomleft = (0 , height))
+        screen.blit(pointsText, pointsText_rect)
 
         # Update window with newly drawn pixels
         pg.display.flip()  
